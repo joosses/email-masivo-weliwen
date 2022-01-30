4cecc208804b544afb9e666277d3a9853b9839f8
Mateh Elismar <matematicoelismar@gmail.com>
2022-01-29 23:48:15 -0400

    feat: added email-template to render and send styled emails. todo: move all images in assets to a remote cloud service or cdn.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { sendMail } = require('./services/nodemailer');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(require('./routes/index'));
@@ -11,3 +12,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
 console.log('Server on port 3000')
+
+sendMail({
+    email: "joossesp@gmail.com",
+    template: "report",
+
+}).then(x => console.log('mail sent'))
